cfb531b699649d34b595c3a10d92ebb363e4bd00
mozartl2 <lucas.mozsart@gmail.com>
2025-09-05 09:06:02 -0300

    Novo

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.trocar-imagem').forEach(function(btn) {
+    const imagens = JSON.parse(btn.getAttribute('data-imagens'));
+    let idx = 0;
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        const img = btn.closest('.card').querySelector('.produto-img');
+        idx = (idx + 1) % imagens.length;
+        img.src = imagens[idx];
+    });
+});
